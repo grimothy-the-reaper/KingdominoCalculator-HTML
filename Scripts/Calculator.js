ce66f9af514d9a5d50bd13cab47efcfd585a752e
Dave <daveyas30@live.com>
2018-02-26 13:25:59 -0600

    Initial commit

---
+++ Scripts/Calculator.js
@@ -0,0 +1,46 @@
+﻿function Increment(terrain, type) {
+    var input = '#input-' + terrain + '-' + type;
+    $(input).text(GetCurrentValue(terrain, type) + 1);
+    GetTotal(terrain);
+}
+
+function Decrement(terrain, type) {
+    var input = '#input-' + terrain + '-' + type;
+    if (GetCurrentValue(terrain, type) === 0) {
+        $(input).val(0);
+    }
+    else {
+        $(input).val(GetCurrentValue(terrain, type) - 1);
+    }
+    GetTotal(terrain);
+}
+
+function GetCurrentValue(terrain, type) {
+    return GetInt('#input-' + terrain + '-' + type);
+}
+
+function GetTotal(terrain) {
+    var total = '#total-' + terrain;
+    $(total).text(GetCurrentValue(terrain, 'tile') * GetCurrentValue(terrain, 'crown'));
+    GetGrandTotal();
+}
+
+function GetGrandTotal() {
+    var temp = GetCurrentGrandTotalValue();
+    $('span[id^="total"]').each(function () {
+        temp += parseInt($(this).text())
+    });
+    $('#grand-total').text(temp);
+}
+
+function GetCurrentGrandTotalValue() {
+    var input = '#grand-total';
+    return GetInt('#grand-total');
+}
+
+function GetInt(control) {
+    if (!$.isNumeric($(control).text())) {
+        return 0;
+    }
+    return parseInt($(control).text());
+}
